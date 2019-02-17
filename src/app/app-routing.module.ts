512037a0fb6ce8f8e8eaea241072d9dd2dd54852
nait said samir <naitsaid_2002@yahoo.fr>
2019-02-17 14:01:40 -0500

    ajout de component d'authentification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
   { path: 'movies/:id', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule' },
+  { path: 'login', loadChildren: './pages/public/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/public/register/register.module#RegisterPageModule' },
+  { path: 'dashboard', loadChildren: './pages/membres/dashboard/dashboard.module#DashboardPageModule' },
 ];
 
 @NgModule({
